86f0c3201dcddfb54d72839526a7b92cb0e1c9fb
Mateus Zuffo <mateushmdz@gmail.com>
2021-07-29 14:14:48 -0300

    Estilização da Listagem

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,11 +1,7 @@
 import { RepositoryList } from './components/RepositoryList';
-import { Counter } from './components/Counter';
 import './styles/global.scss';
 export function App(){
     return (
-        <>
-            <RepositoryList />
-            <Counter />
-        </>
+    <RepositoryList />
     )
 }
