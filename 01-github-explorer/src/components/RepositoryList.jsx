d93b636d93f77c20bd082849b8c539ac38e61d74
Mateus Zuffo <mateushmdz@gmail.com>
2021-07-29 15:23:33 -0300

    utilizando useEffect

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
 
 const repository= {
     name: "teste",
@@ -7,6 +8,13 @@ const repository= {
     link: "https://github.com/mateus-zuffo"
 }
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([]);
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data));
+    },[]);
+
     return(
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
